d7806441a14d5db9331693206774875cf70c8fcd
cyril0124 <211998078@qq.com>
2025-10-22 19:49:28 +0800

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,5 +3,8 @@ local config = {}
 
 -- config.color_scheme = 'Batman'
 config.color_scheme = 'Tokyo Night'
+config.font_size = 14.0
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 30
 
 return config
