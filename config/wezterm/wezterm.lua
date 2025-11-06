1dc639d3c75ecd980cbc7b4c5083817ede1c0ecb
cyril0124 <211998078@qq.com>
2025-11-06 14:44:10 +0800

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,4 +7,10 @@ config.font_size = 14.0
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 30
 
+config.font = wezterm.font_with_fallback({
+    'JetBrainsMono NF',
+    -- 'PingFang SC', -- 中文字体, 需要另外安装
+    'Heiti SC',
+})
+
 return config
