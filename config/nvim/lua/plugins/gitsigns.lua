9b99b2cb6773de1d66394ce55ee78de7f5b724f6
cyril0124 <211998078@qq.com>
2026-03-16 17:35:58 +0800

    update

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -4,9 +4,20 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
         require('gitsigns').setup({
+            signs = {
+                add          = { text = '▎' },
+                change       = { text = '▎' },
+                delete       = { text = '▁' },
+                topdelete    = { text = '▔' },
+                changedelete = { text = '▎' },
+                untracked    = { text = '┆' },
+            },
+            -- Color line numbers for changed lines
+            numhl = true,
+            current_line_blame = false,
             current_line_blame_opts = {
                 delay = 100,
-            }
+            },
         })
     end,
 }
