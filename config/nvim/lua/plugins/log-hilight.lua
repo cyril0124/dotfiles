4d06be5cd5c0cc81662e22560041aa803c1bec82
cyril0124 <211998078@qq.com>
2026-03-16 14:35:59 +0800

    update

--- config/nvim/lua/plugins/log-hilight.lua
+++ config/nvim/lua/plugins/log-hilight.lua
@@ -1,5 +1,19 @@
 -- https://github.com/fei6409/log-highlight.nvim
 return {
-    'fei6409/log-highlight.nvim',
-    opts = {},
+    {
+        'fei6409/log-highlight.nvim',
+        opts = {},
+    },
+
+    -- https://github.com/0xferrous/ansi.nvim
+    {
+        '0xferrous/ansi.nvim',
+        cmd = { "AnsiEnable", "AnsiDisable", "AnsiToggle" },
+        config = function()
+            require('ansi').setup({
+                auto_enable = false,
+                filetypes = { 'log', 'ansi' },
+            })
+        end,
+    },
 }
