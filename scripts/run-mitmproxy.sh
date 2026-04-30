8717ade9fdf1470e2234c2e6e103d917d9427930
cyril0124 <211998078@qq.com>
2026-05-01 02:27:58 +0800

    chore: add mitmproxy utility scripts

---
+++ scripts/run-mitmproxy.sh
@@ -0,0 +1,1 @@
+mitmproxy --mode upstream:$http_proxy
---
+++ scripts/setup-mitmproxy.sh
@@ -0,0 +1,8 @@
+PORT=8080
+export http_proxy="http://127.0.0.1:$PORT"
+export https_proxy="http://127.0.0.1:$PORT"
+export HTTP_PROXY="http://127.0.0.1:$PORT"
+export HTTPS_PROXY="http://127.0.0.1:$PORT"
+export NO_PROXY=""
+export SSL_CERT_FILE="$HOME/.mitmproxy/mitmproxy-ca-cert.pem"
+
