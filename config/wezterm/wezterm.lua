69d3f57ae36748b767e7c2b51d2927dee061173a
cyril0124 <211998078@qq.com>
2026-08-04 09:31:27 +0800

    fix(wezterm): set term and disable translucent background

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,11 +1,12 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.term = 'wezterm'
 -- config.color_scheme = 'Batman'
 config.color_scheme = 'Tokyo Night'
 config.font_size = 14.0
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 30
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 30
 
 config.font = wezterm.font_with_fallback({
     'JetBrains Mono NL',
